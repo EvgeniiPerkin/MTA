3cc68a0bc4a12f4a84e60efbd657f96fc7aa5df1
user <user@dev>
2022-04-22 22:30:56 +0300

    дооформил документацию класса заказа
    дооформил документацию класса заказа

--- MTA/src/main/java/com/perkin/dev/domain/Order.java
+++ MTA/src/main/java/com/perkin/dev/domain/Order.java
@@ -140,138 +140,258 @@ public class Order {
 		this.orderId = orderId;
 	}
 
+	/**
+	 * возвращает дату заказа
+	 * @return дата заказа
+	 */
 	public Date getDateOrder() {
 		return dateOrder;
 	}
-
+	/**
+	 * устанавливает дату заказа
+	 * @param dateOrder дата заказа
+	 */
 	public void setDateOrder(Date dateOrder) {
 		this.dateOrder = dateOrder;
 	}
-
+	
+	/**
+	 * возвращает номер заказа
+	 * @return номер заказа
+	 */
 	public Long getOrderNumber() {
 		return orderNumber;
 	}
-
+	/**
+	 * устанавливает номер заказа
+	 * @param orderNumber номер заказа
+	 */
 	public void setOrderNumber(Long orderNumber) {
 		this.orderNumber = orderNumber;
 	}
-
+	
+	/**
+	 * возвращает вес груза
+	 * @return вес груза
+	 */
 	public Double getCargoWeight() {
 		return cargoWeight;
 	}
-
+	/**
+	 * устанавливает вес груза
+	 * @param cargoWeight вес груза
+	 */
 	public void setCargoWeight(Double cargoWeight) {
 		this.cargoWeight = cargoWeight;
 	}
 
+	/**
+	 * возвращает обём
+	 * @return объём
+	 */
 	public Double getVolume() {
 		return volume;
 	}
-
+	/**
+	 * устанавливает объём
+	 * @param volume объём
+	 */
 	public void setVolume(Double volume) {
 		this.volume = volume;
 	}
 
+	/**
+	 * возвращает дату погрузки
+	 * @return дата погрузки
+	 */
 	public Date getLoadingDate() {
 		return loadingDate;
 	}
-
+	/**
+	 * устанавливает дату погрузки
+	 * @param loadingDate дата погрузки
+	 */
 	public void setLoadingDate(Date loadingDate) {
 		this.loadingDate = loadingDate;
 	}
 
+	/**
+	 * возвращает дату выгрузки
+	 * @return дата выгрузки
+	 */
 	public Date getUnloadingDate() {
 		return unloadingDate;
 	}
-
+	/**
+	 * устанавливает дату выгрузки
+	 * @param unloadingDate дата выгрузки
+	 */
 	public void setUnloadingDate(Date unloadingDate) {
 		this.unloadingDate = unloadingDate;
 	}
-
+	
+	/**
+	 * возвращает время погрузки
+	 * @return время погрузки
+	 */
 	public Date getLoadingTime() {
 		return loadingTime;
 	}
-
+	/**
+	 * устанавливает время погрузки 
+	 * @param loadingTime время погрузки
+	 */
 	public void setLoadingTime(Date loadingTime) {
 		this.loadingTime = loadingTime;
 	}
-
+	
+	/**
+	 * возвращает время выгрузки ОТ
+	 * @return время выгрузки ОТ
+	 */
 	public Date getUnloadingTimeFrom() {
 		return unloadingTimeFrom;
 	}
-
+	/**
+	 * устанавливает время выгрузки ОТ
+	 * @param unloadingTimeFrom время выгрузки ОТ
+	 */
 	public void setUnloadingTimeFrom(Date unloadingTimeFrom) {
 		this.unloadingTimeFrom = unloadingTimeFrom;
 	}
-
+	
+	/**
+	 * возвращает время выгрузки ПО
+	 * @return время выгрузки ПО
+	 */
 	public Date getUnloadingTimeTo() {
 		return unloadingTimeTo;
 	}
-
+	/**
+	 * устанавливает время выгрузки ПО
+	 * @param unloadingTimeTo время выгрузки ПО
+	 */
 	public void setUnloadingTimeTo(Date unloadingTimeTo) {
 		this.unloadingTimeTo = unloadingTimeTo;
 	}
-
+	
+	/**
+	 * возвращает стоимость
+	 * @return стоимость
+	 */
 	public Double getCost() {
 		return cost;
 	}
-
+	/**
+	 * устанавливает стоимость
+	 * @param cost стоимость
+	 */
 	public void setCost(Double cost) {
 		this.cost = cost;
 	}
-
+	
+	/**
+	 * возвращает описание платежа
+	 * @return описание платежа
+	 */
 	public String getPaymentTerms() {
 		return paymentTerms;
 	}
-
+	/**
+	 * устанавливает описание платежа
+	 * @param paymentTerms описание платежа
+	 */
 	public void setPaymentTerms(String paymentTerms) {
 		this.paymentTerms = paymentTerms;
 	}
-
+	
+	/**
+	 * возвращает автомобиль
+	 * @return автомобиль
+	 */
 	public Car getCar() {
 		return car;
 	}
-
+	/**
+	 * устанавливает автомобиль
+	 * @param car автомобиль
+	 */
 	public void setCar(Car car) {
 		this.car = car;
 	}
-
+	
+	/**
+	 * возвращает груз
+	 * @return груз
+	 */
 	public Cargo getCargo() {
 		return cargo;
 	}
-
+	/**
+	 * устанавливает груз 
+	 * @param cargo груз
+	 */
 	public void setCargo(Cargo cargo) {
 		this.cargo = cargo;
 	}
-
+	
+	/**
+	 * возвращает контрагента
+	 * @return контрагент
+	 */
 	public Counterparty getCounterparty() {
 		return counterparty;
 	}
-
+	/**
+	 * устанавливает контрагента
+	 * @param counterparty контрагент
+	 */
 	public void setCounterparty(Counterparty counterparty) {
 		this.counterparty = counterparty;
 	}
 
+	/**
+	 * возвращает заказчика
+	 * @return заказчик
+	 */
 	public Customer getCustomer() {
+	 
 		return customer;
 	}
-
+	/**
+	 * устанавливает заказчика
+	 * @param customer заказчик
+	 */
 	public void setCustomer(Customer customer) {
 		this.customer = customer;
 	}
 
+	/**
+	 * возвращает водителя
+	 * @return водитель
+	 */
 	public Driver getDriver() {
 		return driver;
 	}
-
+	/**
+	 * устанавливает водителя
+	 * @param driver водитель
+	 */
 	public void setDriver(Driver driver) {
 		this.driver = driver;
 	}
-
+	
+	/**
+	 * возвращает исполнителя
+	 * @return исполнитель
+	 */
 	public Executor getExecutor() {
 		return executor;
 	}
-	// TO DO: дописать описание методов
+	/**
+	 * устанавливает исполнителя
+	 * @param executor исполнитель
+	 */
 	public void setExecutor(Executor executor) {
 		this.executor = executor;
 	}
